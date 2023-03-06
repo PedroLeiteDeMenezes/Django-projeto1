f2e9fce9e7795d524970e4647340d60c96479654
PedroLeiteDeMenezes <pedromenzes2013@outlook.com>
2023-03-06 15:37:48 -0300

    Contact removed

--- projeto/urls.py
+++ projeto/urls.py
@@ -17,11 +17,6 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 
-
-def my_view(request):
-    return HttpResponse('Uma Linda String')
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', my_view),
